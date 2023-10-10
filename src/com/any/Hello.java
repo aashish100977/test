9ac4c4f06af8c8490d040e8be289a224e1e902de
Aashish Khanal <aakh866699@gmail.com>
2023-10-09 17:56:06 -0700

    day 4

--- src/com/any/Hello.java
+++ src/com/any/Hello.java
@@ -6,6 +6,7 @@ public class Hello {
 		System.out.println("I am learning Git"); //First Commits
 		System.out.println("day 2");
 		System.out.println("day 3");
+		System.out.println("day 4");
 	}
 
 }
