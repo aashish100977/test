cd0a21789e2f31188feb728e2426d7931604acfd
Aashish Khanal <aakh866699@gmail.com>
2023-10-10 17:17:00 -0700

    commit from develop

--- src/com/any/Hello.java
+++ src/com/any/Hello.java
@@ -6,7 +6,7 @@ public class Hello {
 		System.out.println("I am learning Git"); //First Commits
 		System.out.println("day 2");
 		System.out.println("day 3");
-		System.out.println("day 4");
+		System.out.println("Done in develop branch");
 	}
 
 }
